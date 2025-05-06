7c295b9ddb8cdc0d60896e5f11195d1a757d53e0
padmavathi-cm <ellapupadma@gmail.com>
2025-05-06 16:07:29 +0530

    first

---
+++ src/main/java/com/sanders/build/prj/BuildPrjApplication.java
@@ -0,0 +1,13 @@
+package com.sanders.build.prj;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BuildPrjApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BuildPrjApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sanders/build/prj/BuildPrjApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sanders.build.prj;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BuildPrjApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
